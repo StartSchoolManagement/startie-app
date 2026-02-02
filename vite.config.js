09d836f3ab85a82de33433f121fa2613a45d924d
Eliass Baranovs <eliass.baranovs@gmail.com>
2026-02-02 14:01:41 +0200

    ANON

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  root: '.', // Project root
-  publicDir: 'assets', // Static assets (images, sounds)
+  root: ".", // Project root
+  publicDir: "assets", // Static assets (images, sounds)
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        play: resolve(__dirname, 'play.html'),
-        highscores: resolve(__dirname, 'highscores.html'),
-        admin: resolve(__dirname, 'admin.html'),
+        main: resolve(__dirname, "index.html"),
+        play: resolve(__dirname, "play.html"),
+        highscores: resolve(__dirname, "highscores.html"),
+        admin: resolve(__dirname, "admin.html"),
       },
     },
   },
